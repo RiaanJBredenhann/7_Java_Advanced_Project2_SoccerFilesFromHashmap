7fc33488773b7dbc182bb67871065238c3010256
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-05-23 11:07:43 +0200

    update

---
+++ Hash_Map_Demo.java
@@ -0,0 +1,27 @@
+// Java code to illustrate the get() method 
+import java.util.*;
+
+public class Hash_Map_Demo {
+    public static void main(String[] args)
+    {
+
+        // Creating an empty HashMap 
+        HashMap<Integer, String> hash_map = new HashMap<Integer, String>();
+
+        // Mapping string values to int keys 
+        hash_map.put(10, "Geeks");
+        hash_map.put(15, "4");
+        hash_map.put(20, "Geeks");
+        hash_map.put(25, "Welcomes");
+        hash_map.put(30, "You");
+
+        // Displaying the HashMap 
+        System.out.println("Initial Mappings are: " + hash_map);
+
+        // Getting the value of 25 
+        System.out.println("The Value is: " + hash_map.get(25));
+
+        // Getting the value of 10 
+        System.out.println("The Value is: " + hash_map.get(10));
+    }
+} 
