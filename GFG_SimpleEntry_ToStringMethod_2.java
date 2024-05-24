5ac89574239bc531eddad4781d54a2c35de7358d
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-05-24 10:55:27 +0200

    update

---
+++ GFG_SimpleEntry_ToStringMethod_2.java
@@ -0,0 +1,45 @@
+// Java program to demonstrate
+// AbstractMap.SimpleEntry.toString() method
+
+import java.util.*;
+
+public class GFG_SimpleEntry_ToStringMethod_2 {
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static void main(String[] args)
+    {
+
+        // create a ArrayList of Map
+        ArrayList<AbstractMap
+                .SimpleEntry<String, String> >
+                arrayList
+                = new ArrayList<AbstractMap
+                .SimpleEntry<String, String> >();
+
+        // add values
+        arrayList.add(new AbstractMap
+                .SimpleEntry(" 001AB ", " Emp 1"));
+        arrayList.add(new AbstractMap
+                .SimpleEntry(" 011AC ", " Emp 2"));
+        arrayList.add(new AbstractMap
+                .SimpleEntry(" 111AD ", " Emp 3"));
+        arrayList.add(new AbstractMap
+                .SimpleEntry(" 101BE ", " Emp 4"));
+        arrayList.add(new AbstractMap
+                .SimpleEntry(" 110CE ", " Emp 5"));
+
+        // print keys
+        for (int i = 0; i < arrayList.size(); i++) {
+
+            // get map from list
+            AbstractMap.SimpleEntry<String, String>
+                    map
+                    = arrayList.get(i);
+
+            // get representation of map using toString()
+            String value = map.toString();
+
+            System.out.println(value);
+        }
+    }
+}
